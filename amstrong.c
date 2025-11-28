fdfb6afc7a5e8b099a1b1014f4dc4fa7211fb792
Ramcharan Sai <25r11a05ff@gcet.edu.in>
2025-11-28 14:40:03 +0530

    Refactor Armstrong number check into separate function

--- amstrong.c
+++ amstrong.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 #include<math.h>
+int amst_c(int);
 int main(){
-    int n;
+    int n,am;
     printf("Enter the number:\n");
     scanf("%d",&n);
-    int d=0,m,sum=0,r;
+    am=amst_c(n);
+if(am==n)
+    printf("%d is an Armstrong number.\n",n);
+else
+    printf("%d is not an Armstrong number.\n",n);
+return 0;
+}
+int amst_c(int n){
+     int d=0,m,sum=0,r;
     m=n;
     while(n>0){
        n=n/10;
@@ -16,9 +25,5 @@ while(n>0){
     sum=sum+pow(r,d);
     n=n/10;
 }
-if(sum==m)
-    printf("%d is an Armstrong number.\n",m);
-else
-    printf("%d is not an Armstrong number.\n",m);
-return 0;
+return sum;
 }
